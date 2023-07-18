0d532b41d99519ff34c134675853fd3aaf07fc9d
syedfaheem27 <syedfaheem2719@gmail.com>
2023-07-18 23:21:32 +0530

    Add a script.js and add the theme-toggle functionality

---
+++ src/js/script.js
@@ -0,0 +1,30 @@
+const lightIcon = document.getElementById("theme-toggle-light-icon");
+const darkIcon = document.getElementById("theme-toggle-dark-icon");
+const themeToggleButton = document.getElementById("theme-toggle");
+
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark").matches)
+) {
+  document.documentElement.classList.add("dark");
+  lightIcon.classList.remove("hidden");
+} else {
+  document.documentElement.classList.remove("dark");
+  darkIcon.classList.remove("hidden");
+}
+
+function toggleTheme() {
+  lightIcon.classList.toggle("hidden");
+  darkIcon.classList.toggle("hidden");
+
+  if (document.documentElement.classList.contains("dark")) {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("color-theme", "");
+  } else {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("color-theme", "dark");
+  }
+}
+
+themeToggleButton.addEventListener("click", toggleTheme);
